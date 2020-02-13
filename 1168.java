890056ba4caf94380c71736c685157551f68a13a
Eujin Kim <52485293+mingmeng030@users.noreply.github.com>
2020-02-13 16:44:51 +0900

    Create 1168.java

---
+++ 1168.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args) {
+        Scanner scan2=new Scanner(System.in);
+        int N=scan2.nextInt();
+        int K=scan2.nextInt();
+        StringBuilder SB = new StringBuilder();
+        Vector<Integer> v=new Vector<Integer>();
+        
+        for(int i=1;i<=N ;i++) v.add(i);
+        
+        int now=K-1; //현재 위치==now
+        while(true) {
+            SB.append(v.get(now));
+            v.remove(now);
+            if(v.size()==0) break;
+            SB.append(", ");
+            now+=K-1;
+            now%=v.size();
+        }
+        System.out.println("<"+SB+">");
+    }
+}
