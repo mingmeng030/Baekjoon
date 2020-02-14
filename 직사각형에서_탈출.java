959586e12555b919dca8e4e8417f9fee7b229574
mingmeng030 <djwls_0716@naver.com>
2020-02-14 17:52:58 +0900

    -

---
+++ 직사각형에서_탈출.java
@@ -0,0 +1,22 @@
+package 백준_1월;
+import java.util.*;
+public class 직사각형에서_탈출 {
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		int x=scan.nextInt();
+		int y=scan.nextInt();
+		int w=scan.nextInt();
+		int h=scan.nextInt();
+		
+		int left=x;
+		int right=h-y;
+		int up=w-x;
+		int down=y;
+		
+		int[] temp= {left,right,up,down};
+		int result=temp[0];
+		for(int i=0; i<temp.length;i++) if(temp[i]<result) result=temp[i];
+		
+		System.out.println(result);
+	}
+}
---
+++ 하얀칸.java
@@ -0,0 +1,18 @@
+package ¹éÁØ_1¿ù;
+import java.util.*;
+public class ÇÏ¾áÄ­ {
+	public static void main(String[] args) {
+		Scanner scan= new Scanner(System.in);
+		int horse=0;
+		String[] chess=new String[8];
+		for(int i=0; i<8; i++) chess[i]=scan.nextLine();
+		for(int i=0; i<8; i++) {
+			for(int j=0; j<8; j++) {
+				if(i%2==0&&j%2==0&&chess[i].charAt(j)=='F') horse++;
+				else if(i%2==1&&j%2==1&&chess[i].charAt(j)=='F') horse++;
+				else continue;
+			}
+		}
+		System.out.println(horse);
+	}
+}
