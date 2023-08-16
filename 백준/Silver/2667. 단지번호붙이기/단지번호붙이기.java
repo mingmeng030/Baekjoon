3e3e6817101cb30adc1d1acd699a300e6564441d
Eujin Kim <52485293+mingmeng030@users.noreply.github.com>
2023-08-16 20:14:10 +0900

    [Silver I] Title: 단지번호붙이기, Time: 92 ms, Memory: 11668 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -7,7 +7,7 @@ public class Main{
     static boolean[][] visited; 
     static int n, count=0; 
     static List<Integer> apartmentComplex = new ArrayList<>(); 
-	static int[] dx = { -1, 1, 0, 0 };
+    static int[] dx = { -1, 1, 0, 0 };
     static int[] dy = { 0, 0, -1, 1 }; 
     
     public static void main(String[] args) throws IOException {
@@ -53,12 +53,11 @@ public class Main{
                 int nextX = nowX + dx[i];
 				int nextY = nowY + dy[i];  
                 
-                if (nextX < n && nextY < n && nextX >= 0 && nextY >= 0){
-                    if (!visited[nextX][nextY] && map[nextX][nextY] == 1){
-                        visited[nextX][nextY] = true;
-                        queue.offer(new int[] {nextX, nextY});
-                        house++;
-                    }
+                if (nextX < n && nextY < n && nextX >= 0 && nextY >= 0
+                   && !visited[nextX][nextY] && map[nextX][nextY] == 1){
+                    visited[nextX][nextY] = true;
+                    queue.offer(new int[] {nextX, nextY});
+                    house++;
                 }
             }
         }
