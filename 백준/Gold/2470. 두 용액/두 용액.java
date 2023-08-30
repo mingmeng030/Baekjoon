1e70490b229b6fe52b86bbaa4f1b5b19b6291829
Eujin Kim <52485293+mingmeng030@users.noreply.github.com>
2023-08-30 15:51:37 +0900

    [Gold V] Title: 두 용액, Time: 276 ms, Memory: 32344 KB -BaekjoonHub

---
+++ 백준/Gold/2470. 두 용액/두 용액.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        
+        int[] arr = new int[n];
+        String[] line = br.readLine().split(" ");
+        for(int i=0; i<n; i++){
+            arr[i] = Integer.parseInt(line[i]);
+        }
+        Arrays.sort(arr);
+        int left=0, right=n-1;
+        int resultLeft = 0, resultRight = n-1;
+        int min = Integer.MAX_VALUE;
+        
+        while(left<right){
+            int sum = arr[left]+arr[right];
+			if(min > Math.abs(sum)) {
+				min = Math.abs(sum);
+				resultLeft = left; 
+                resultRight = right;
+			}
+			if(sum>=0) right--;	
+			else left++;
+        }
+        System.out.println(arr[resultLeft] + " " + arr[resultRight]);
+    }
+}
