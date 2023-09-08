3aeb66b9797d64e18262d8d097b1374fbcb85441
Eujin Kim <52485293+mingmeng030@users.noreply.github.com>
2023-09-08 14:16:45 +0900

    [Silver I] Title: 점프, Time: 84 ms, Memory: 11820 KB -BaekjoonHub

--- 백준/Silver/1890. 점프/점프.java
+++ 백준/Silver/1890. 점프/점프.java
@@ -1,30 +1,37 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        
-        int[][] board = new int[n+1][n+1];
-        for(int i=0; i<n; i++){
-            String[] line = br.readLine().split(" ");
-            for(int j=0; j<n; j++)
-                board[i+1][j+1] = Integer.parseInt(line[j]);
-        }
 
-        long[][] dp = new long[n+1][n+1]; 
-        dp[1][1]=1;
-            
-        for(int i=1; i<=n; i++){
-            for(int j=1; j<=n; j++){
-                int now = board[i][j];
-                if(now == 0) break;
-                if(j+now<=n) dp[i][j+now] += dp[i][j];
-                if(i+now<=n) dp[i+now][j] += dp[i][j];
+        int[][] arr = new int[n][n];
+        long[][] dp = new long[n][n];
+
+        for(int i=0; i<n; i++){
+            String[] line= br.readLine().split(" ");
+            for(int j=0; j<n; j++){
+                arr[i][j] = Integer.parseInt(line[j]);
             }
-        }   
-        System.out.println(dp[n][n]);
+        }
+        
+        dp[0][0] = 1;
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                int now = arr[i][j];
+                if(now == 0) break;
+                if(j+now < n){
+                    dp[i][j+now] += dp[i][j];
+                }
+                if(i+now < n){
+                    dp[i+now][j] += dp[i][j];
+                }
+            }
+        }
+        System.out.print(dp[n-1][n-1]);
     }
 }
