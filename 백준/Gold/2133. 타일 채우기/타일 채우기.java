fffa49a423267915bb88640616c84c5bbfb01efb
Eujin Kim <52485293+mingmeng030@users.noreply.github.com>
2023-09-08 16:34:16 +0900

    [Gold IV] Title: 타일 채우기, Time: 80 ms, Memory: 11492 KB -BaekjoonHub

--- 백준/Gold/2133. 타일 채우기/타일 채우기.java
+++ 백준/Gold/2133. 타일 채우기/타일 채우기.java
@@ -3,20 +3,22 @@ import java.io.InputStreamReader;
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        int[] tile = new int[N + 1];
+        int n = Integer.parseInt(br.readLine());
+        int[] wall = new int[n+1];
         
-        if (N % 2 == 0) {
-            tile[0] = 1;
-            tile[2] = 3;
-            for (int i=4; i<=N; i += 2) {
-                tile[i] = tile[i-2]*tile[2];
-                for (int j = i-4; j >= 0; j -= 2) 
-                    tile[i] += tile[j]*2;
+        if(n%2==0){
+            wall[0] = 1;
+            wall[2] = 3;
+            for(int i = 4; i<=n; i+=2){
+                wall[i] += wall[i-2]*wall[2];
+                for(int j = i-4; j>=0; j-=2){
+                    wall[i] += wall[j]*2;
+                }
             }
+            System.out.print(wall[n]);
         }
-        System.out.println(N%2==0 ? tile[N] : 0);
+        else System.out.print(0);
     }
 }
